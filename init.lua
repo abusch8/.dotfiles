9595442a65c916312c3273220d8d8cdd5a544de7
abusch8 <abusch8@huskers.unl.edu>
2023-10-03 00:58:02 -0500

    Updated settings.lua

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("chambers/lazy")
 require("chambers/settings")
 require("chambers/ui")
 require("chambers/plugins/cmp")
+require("chambers/plugins/gitsigns")
 require("chambers/plugins/telescope")
 require("chambers/plugins/tree")
 require("chambers/plugins/treesitter")
@@ -10,8 +11,5 @@ require("chambers/plugins/treesitter")
 local lspconfig = require("lspconfig")
 lspconfig.tsserver.setup({})
 lspconfig.rust_analyzer.setup({})
-lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } },
-        },
-    },
-})
+lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
 
---
+++ lua/chambers/settings.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+
