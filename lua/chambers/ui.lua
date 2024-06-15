9042d8cd47679e327bcb180c88718174a61b7b6d
abusch8 <abusch8@huskers.unl.edu>
2024-06-15 14:33:57 -0500

    Tweaked EndOfBuffer highlight

--- lua/chambers/ui.lua
+++ lua/chambers/ui.lua
@@ -3,9 +3,19 @@ local tokyonight = require("tokyonight")
 
 nonicons.setup({})
 
+local function on_highlights(hl, c)
+    hl.EndOfBuffer = {
+        fg = hl.LineNr.fg,
+    }
+    hl.NvimTreeEndOfBuffer = {
+        fg = c.bg,
+    }
+end
+
 tokyonight.setup({
     style = "storm", -- { storm, night, moon, day }
     lualine_bold = true,
+    on_highlights = on_highlights,
 })
 
 vim.cmd[[ colorscheme tokyonight ]]
